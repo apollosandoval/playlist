b66886c799606c515f040b7eb3ea1adcef6ebcc7
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-10-04 15:50:00 -0700

    generating GET request

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 (function() {
-    
+    let albumUrl = 'https://lit-fortress-6467.herokuapp.com/object';
+    let playlistUrl = '';
+
+    axios.get(albumUrl)
+        .then ( response => {
+            let albums = response.data.results;
+            
+            let randomAlbums = noRepeatCovers(albums);
+            console.log(randomAlbums);
+        })
+        .catch( err => {
+            console.log(err);
+        })
+
+    function noRepeatCovers(albumArray) {
+        let randomCollection = [];
+        let randomIndex = [];
+
+        while (randomCollection.length < 3) {
+            let randomNumber = Math.floor(Math.random() * albumArray.length);
+            if (!randomIndex.includes(randomNumber)) {
+                randomIndex.push(randomNumber);
+                randomCollection.push(albumArray[randomNumber]);
+            }
+        }
+        return randomCollection;
+    }
+
+    function populateTracks(albumArray) {
+        
+    }
 })();
